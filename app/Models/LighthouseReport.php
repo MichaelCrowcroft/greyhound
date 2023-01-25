c178ff49ed14e8eee59c82544da30c046bb6bb4e
Michael Crowcroft <michaelcrowcroft@outlook.com>
2023-01-25 13:30:02 -0500

    added lighthouse scores on the company show page

--- app/Models/LighthouseReport.php
+++ app/Models/LighthouseReport.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class LighthouseReport extends Model
 {
@@ -12,8 +13,15 @@ class LighthouseReport extends Model
 
     protected $guarded = [];
 
+    protected $with = ['lighthouseReportData'];
+
     public function company(): BelongsTo
     {
         return $this->belongsTo(Company::class);
+    }
+
+    public function lighthouseReportData(): HasMany
+    {
+        return $this->hasMany(LighthouseReportData::class);
     }
 }
--- resources/js/Pages/Companies/Show.vue
+++ resources/js/Pages/Companies/Show.vue
@@ -32,9 +32,12 @@ const editingCompanyName = ref(false);
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                 <div v-for="lighthouse_reports in company.lighthouse_reports" :key="lighthouse_reports.id"
                         class="w-full mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                    <Link :href="'/companies/'">
+                    <Link :href="'/companies/' + company.id + '/lighthouse-report/' +lighthouse_reports.id">
                         {{ lighthouse_reports.url }}
                     </Link>
+                    <p v-for="lighthouse_report_data in lighthouse_reports.lighthouse_report_data" :key="lighthouse_report_data.id">
+                        {{ lighthouse_report_data.performance }}
+                    </p>
                 </div>
 
                 <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
