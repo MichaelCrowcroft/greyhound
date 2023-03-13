2df04cb2931f28bb0dd013c400b6f25781b3cef8
Michael Crowcroft <michaelcrowcroft@outlook.com>
2023-03-12 22:01:18 -0400

    wip

--- resources/js/Pages/Companies/Partials/IndexCompanyLineChart.vue
+++ resources/js/Pages/Companies/Partials/IndexCompanyLineChart.vue
@@ -3,22 +3,27 @@ import { Line } from 'vue-chartjs'
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip,Legend } from 'chart.js'
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
+ChartJS.defaults.font.family = ['Nunito'];
 
 const props = defineProps({
     data: Object
 })
 
-let datasets = array_combine(
-    Object.values(Object.values(props.data)).map(item => Object.values(item).map(item => item.performance)),
-    Object.keys(props.data)
-)
+const companies = Object.keys(props.data)
+const values = Object.values(Object.values(props.data)).map(item => Object.values(item).map(item => item.performance))
+const colors = ['#13B5EA', '2CA01C', '#0075DD', '#2047CE', '#00D639']
+const datasets = companies.map((company, index) => [{
+    label: company,
+    data: values[index],
+    borderColor: colors[index],
+    backgroundColor: colors[index],
+    tension: 0.1
+}]).flat()
+
 
 const chartData = {
     labels: Object.keys(Object.values(props.data)[0]).map(date => new Date(date).toDateString()),
-    datasets: [{
-        label: 'test',
-        data: Object.values(Object.values(props.data)[2]).map(item => item.performance)
-    }],
+    datasets: datasets
 }
 
 const options = {
@@ -32,6 +37,6 @@ const options = {
 </script>
 
 <template>
-    {{ datasets }}
+    <h3 class="mb-4 text-lg font-semibold text-center">Performance</h3>
     <Line :data="chartData" :options="options" />
 </template>
